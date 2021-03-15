89b5d01f4ceaaa0f0f03b89367aaf212dbb62331
1000ship <cjstjdgur1234@gmail.com>
2021-03-16 00:57:39 +0900

    Updates

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/tech/[id]":["static\u002Fchunks\u002Fpages\u002Ftech\u002F[id].js"],"/work/[id]":["static\u002Fchunks\u002Fpages\u002Fwork\u002F[id].js"],sortedPages:["\u002F_app","\u002F_error","\u002Ftech\u002F[id]","\u002Fwork\u002F[id]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
