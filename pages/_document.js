41e670e29e87fc27ed3b43dac0f1a0855df07d9e
Dante Chun <dev.1000ship@gmail.com>
2022-01-26 00:43:23 +0900

    feat: Waterbrush 작품에 추가

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,15 @@
-import Document, { Main, NextScript } from 'next/document'
-import Head from 'next/head'
+import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
-import { Reset } from 'styled-reset'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
-    
+
     try {
       ctx.renderPage = () =>
         originalRenderPage({ enhanceApp: App => props => sheet.collectStyles(<App {...props} />), })
-      
+
       const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
